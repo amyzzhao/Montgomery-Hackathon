ab24dc901b4e66baeb68244d7f2401dc0d92d7f0
amyzzhao <amy.zzhao1@gmail.com>
2025-05-31 11:43:20 -0400

    save

--- main.py
+++ main.py
@@ -14,13 +14,38 @@ PLAYER_VEL = 5
 BG_IMAGE_ORIG = pygame.image.load(os.path.join("assets", "Background.png"))
 
 
-window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-pygame.display.set_caption("Platformer")
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheet(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+        
+        if direction:
+            all_sprites[image.replace(".png", "")+"_right"] = sprites
+            all_sprites[image.replace(".png", "")+"_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+    
+    return all_sprites
 
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1 
+    SPRITES = load_sprite_sheet("player", "walkingAnimation.png", 32, 32, True)
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -48,14 +73,14 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
         
         self.fall_count += 1
     
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
-
+        self.sprite = self.SPRITES["walkingAnimation_" + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 def draw(window, player):
     player.draw(window)
@@ -74,8 +99,10 @@ def handle_move(player):
 
 def main(window):
     clock = pygame.time.Clock()
-    BG_IMAGE = pygame.transform.scale(BG_IMAGE_ORIG, (WIDTH, HEIGHT))
     win_width, win_height = WIDTH, HEIGHT
+    window = pygame.display.set_mode((win_width, win_height), pygame.RESIZABLE)
+    pygame.display.set_caption("Platformer")
+    BG_IMAGE = pygame.transform.scale(BG_IMAGE_ORIG, (WIDTH, HEIGHT))
 
     player = Player(100, 100, 50, 50)
     
