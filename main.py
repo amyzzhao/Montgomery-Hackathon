d9eabe0bd06224cbc90d223c297fbb63c7ac7754
amyzzhao <amy.zzhao1@gmail.com>
2025-05-31 14:12:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,13 @@ def load_sprite_sheet(dir1, file_name, width, height, direction=False):
 
     return all_sprites
 
+
+def get_block(size):
+    path = join("assets", "Terrain", "Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size)
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1 
@@ -99,12 +106,27 @@ class Player(pygame.sprite.Sprite):
     def draw(self, window):
         window.blit(self.sprite, (self.rect.x, self.rect.y))
 
+
 class Object(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, name=None):
-      super().__init__()
-      self.rect = pygame.Rect(x, y, width, height)
-      self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
     
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = load_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+
 
 def draw(window, player):
     player.draw(window)
