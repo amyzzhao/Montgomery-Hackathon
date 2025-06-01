945a9dfde795e59fb1476f2a18f40191f40ba836
amyzzhao <amy.zzhao1@gmail.com>
2025-05-31 21:02:53 -0400

    qq

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ class Player(pygame.sprite.Sprite):
     def jump(self):
         self.y_vel = -self.GRAVITY * 8
         self.animation_count = 0
+        self.jump_count = 1
         if self.jump_count == 1:
             self.fall_count = 0
 
